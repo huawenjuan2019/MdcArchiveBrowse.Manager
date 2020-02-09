52040512e0d3f4ae578473d2a27d50bc69920387
sunshuo <97662@DESKTOP-RCM7SUP>
2020-02-09 13:04:09 +0800

    修改

--- MdcArchiveBrowse.Manager/Controls/ucApplyDept.Designer.cs
+++ MdcArchiveBrowse.Manager/Controls/ucApplyDept.Designer.cs
@@ -28,24 +28,25 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.listView1 = new System.Windows.Forms.ListView();
+            this.listView = new System.Windows.Forms.ListView();
             this.btnAdd = new System.Windows.Forms.Button();
             this.btnChange = new System.Windows.Forms.Button();
             this.btnDelet = new System.Windows.Forms.Button();
             this.btnClose = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // listView1
+            // listView
             // 
-            this.listView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            this.listView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.listView1.HideSelection = false;
-            this.listView1.Location = new System.Drawing.Point(13, 13);
-            this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(771, 357);
-            this.listView1.TabIndex = 0;
-            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView.HideSelection = false;
+            this.listView.Location = new System.Drawing.Point(13, 13);
+            this.listView.Name = "listView";
+            this.listView.Size = new System.Drawing.Size(771, 357);
+            this.listView.TabIndex = 0;
+            this.listView.UseCompatibleStateImageBehavior = false;
+            this.listView.SelectedIndexChanged += new System.EventHandler(this.listView_SelectedIndexChanged);
             // 
             // btnAdd
             // 
@@ -56,6 +57,7 @@
             this.btnAdd.TabIndex = 1;
             this.btnAdd.Text = "添加";
             this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
             // 
             // btnChange
             // 
@@ -66,6 +68,7 @@
             this.btnChange.TabIndex = 2;
             this.btnChange.Text = "修改";
             this.btnChange.UseVisualStyleBackColor = true;
+            this.btnChange.Click += new System.EventHandler(this.btnChange_Click);
             // 
             // btnDelet
             // 
@@ -76,6 +79,7 @@
             this.btnDelet.TabIndex = 3;
             this.btnDelet.Text = "删除";
             this.btnDelet.UseVisualStyleBackColor = true;
+            this.btnDelet.Click += new System.EventHandler(this.btnDelet_Click);
             // 
             // btnClose
             // 
@@ -95,16 +99,17 @@
             this.Controls.Add(this.btnDelet);
             this.Controls.Add(this.btnChange);
             this.Controls.Add(this.btnAdd);
-            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.listView);
             this.Name = "ucApplyDept";
             this.Size = new System.Drawing.Size(797, 424);
+            this.Load += new System.EventHandler(this.ucApplyDept_Load);
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ListView listView;
         private System.Windows.Forms.Button btnAdd;
         private System.Windows.Forms.Button btnChange;
         private System.Windows.Forms.Button btnDelet;
--- MdcArchiveBrowse.Manager/Controls/ucApplyDept.cs
+++ MdcArchiveBrowse.Manager/Controls/ucApplyDept.cs
@@ -16,7 +16,183 @@ namespace MdcArchiveBrowse.Manager.Controls
             InitializeComponent();
         }
 
+        #region  表格
 
+        /// <summary>
+        /// 新增方法
+        /// </summary>
+        /// <param name="lv"></param>
+        /// <returns></returns>
+        public ListView InsertListView(ListView lv)
+        {
+            ////创建行对象
+            //ListViewItem li = new ListViewItem(_TaskName);
+            ////添加同一行的数据
+            //li.SubItems.Add(_StepStyle);
+            ////将行对象绑定在listview对象中
+            //lv.Items.Add(li);
+            return lv;
+        }
 
+        /// <summary>
+        /// 初始化ListView的方法
+        /// </summary>
+        /// <param name="lv"></param>
+        public void InitListView(ListView lv)
+        {
+            //添加列名
+            ColumnHeader c1 = new ColumnHeader();
+            c1.Width = 200;
+            c1.Text = "编号";
+            ColumnHeader c2 = new ColumnHeader();
+            c2.Width = 170;
+            c2.Text = "申请科室";
+            ColumnHeader c3 = new ColumnHeader();
+            c3.Width = 225;
+            c3.Text = "备注";
+            ColumnHeader c4 = new ColumnHeader();
+            c4.Width = 170;
+            c4.Text = "日期";
+            //设置属性
+            lv.GridLines = true;  //显示网格线
+            lv.FullRowSelect = true;  //显示全行
+            lv.MultiSelect = false;  //设置只能单选
+            lv.View = View.Details;  //设置显示模式为详细
+            lv.HoverSelection = true;  //当鼠标停留数秒后自动选择
+            //把列名添加到listview中
+            lv.Columns.Add(c1);
+            lv.Columns.Add(c2);
+            lv.Columns.Add(c3);
+            lv.Columns.Add(c4);
+        }
+
+        /// <summary>
+        /// 修改的方法
+        /// </summary>
+        /// <param name="lv"></param>
+        /// <returns></returns>
+        public ListView UpdateListView(ListView lv)
+        {
+            if (lv.SelectedItems.Count > 0)
+            {
+                //把修改后的文本框内容添加到listview中
+                //lv.SelectedItems[0].SubItems[0].Text = this.txtTaskName.Text.Trim();
+                //lv.SelectedItems[0].SubItems[1].Text = this.cboStepStyle.Text;
+                MessageBox.Show("任务修改成功！");
+            }
+            return lv;
+        }
+
+        /// <summary>
+        /// 当listview选中状态改变时调用的方法
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void listView_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //当有选择行的数据时
+            if (this.listView.SelectedItems.Count > 0)
+            {
+                //把选择的信息显示在相应的文本框中
+                //this.txtTaskName.Text = this.listView.SelectedItems[0].SubItems[0].Text;
+                //this.cboStepStyle.Text = this.listView.SelectedItems[0].SubItems[1].Text;
+            }
+        }
+
+        /// <summary>
+        /// 增加
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+
+            //if (!String.IsNullOrEmpty(txtTaskName.Text))
+            //{
+            //    if (String.IsNullOrEmpty(cboStepStyle.Text))
+            //    {
+            //        MessageBox.Show("请填写任务步长类型");
+            //        return;
+            //    }
+            //}
+            //else
+            //{
+            //    MessageBox.Show("请填写任务名称");
+            //    return;
+            //}
+            ////获取文本框中的值
+            //_TaskName = this.txtTaskName.Text.Trim();
+            //_StepStyle = this.cboStepStyle.Text;
+            //SelectListView(this.listView);
+            //if (_IsExist == false)
+            //{
+            //    InsertListView(this.listView);
+            //}
+        }
+
+        public void SelectListView(ListView lv)
+        {
+            //列表有数据
+            if (lv.Items.Count > 0)
+            {
+                foreach (ListViewItem li in lv.Items)
+                {
+                    //if (li.SubItems[0].Text == _TaskName)
+                    //{
+                    //    MessageBox.Show("存在该名称");
+                    //    _IsExist = true;
+                    //    return;
+                    //}
+                    //else
+                    //{
+                    //    _IsExist = false;
+                    //}
+                }
+            }
+        }
+
+        /// <summary>
+        /// 修改
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnChange_Click(object sender, EventArgs e)
+        {
+            UpdateListView(this.listView);
+        }
+
+        /// <summary>
+        /// 移除选中行的方法
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnDelet_Click(object sender, EventArgs e)
+        {
+            if (this.listView.SelectedItems.Count > 0)
+            {
+                DialogResult result = MessageBox.Show("确认删除？", "温馨提示", MessageBoxButtons.OKCancel);
+
+                if (result == DialogResult.OK)
+                {
+                    //移除整一行
+                    this.listView.SelectedItems[0].Remove();
+                }
+                else
+                {
+
+                }
+            }
+        }
+        #endregion
+
+        /// <summary>
+        /// 界面初始化
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ucApplyDept_Load(object sender, EventArgs e)
+        {
+            InitListView(listView);
+        }
     }
 }
